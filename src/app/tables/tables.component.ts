76c3e4954daad357bc2302354c3b50a41638d692
Will <doggy.huang@gmail.com>
2020-08-11 12:32:06 +0800

    修正 /tables 頁面的 jQuery 效果

--- src/app/tables/tables.component.ts
+++ src/app/tables/tables.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   templateUrl: './tables.component.html',
   styleUrls: ['./tables.component.css']
@@ -9,6 +11,10 @@ export class TablesComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    // Call the dataTables jQuery plugin
+    $(document).ready(() => {
+      $('#dataTable').DataTable();
+    });
   }
 
 }
